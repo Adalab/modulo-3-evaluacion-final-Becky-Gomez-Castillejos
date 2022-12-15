4e0a912deede0000d2bf12f8c84f8536ca07beea
rebeca <rebecachambers919@gmail.com>
2022-12-15 23:20:27 +0100

    add quality detail 3

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -2,25 +2,25 @@ import CharacterItem from "./CharacterItem";
 
 // Fichero para pintar array
 const CharactersList = ({ characters, filterByName }) => {
-    const CharactersElements = characters.map((character) => {
-      return <CharacterItem character={character} />;
-          
-    });
-  
-    if (characters.length !== 0){
-      return (
-        <section>
-          <ul className='list'>{CharactersElements}</ul>
-        </section>
-          
-      )
-      } else {
-          return(
-              <section>
-                  <p>No existe ningún personaje que coincida con el texto {filterByName}</p>
-              </section>
-          )
-      }
-  };
-  
-  export default CharactersList;
+  const CharactersElements = characters.map((character) => {
+    return <CharacterItem character={character} />;
+  });
+
+  if (characters.length === 0) {
+    return (
+      <section>
+        <p>
+          No existe ningún personaje que coincida con el texto {filterByName}
+        </p>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <ul className="list">{CharactersElements}</ul>
+      </section>
+    );
+  }
+};
+
+export default CharactersList;
