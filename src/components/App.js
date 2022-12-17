f8f76cdedf5beedb3e57c94bb6973772a0e22b51
rebeca <rebecachambers919@gmail.com>
2022-12-17 14:03:30 +0100

    add interview changes 4

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ function App() {
   const [dataApi, setDataApi] = useState([]);
   const [filterByName, setFilterByName] = useState("");
   const [filterByEpisode, setFilterByEpisode] = useState(0);
-  
 
   useEffect(() => {
     getDataFromAPI().then((cleanData) => {
@@ -20,29 +19,23 @@ function App() {
 
   const handleNumberEpisode = (ev) => {
     setFilterByEpisode(ev);
-  }
+  };
 
   const handleFilterName = (value) => {
     setFilterByName(value);
   };
 
-  
-
   const CharactersFiltered = dataApi
-  .filter((character) => {
-    
-      return character.name.toLowerCase().includes(filterByName.toLowerCase())
-  
-  }) .filter((character) => {
-    if(filterByEpisode === 0) {
-      return true
-    } else {
-      return character.episodes === parseInt(filterByEpisode)
-    }
-    
-  }
-  );
-
+    .filter((character) => {
+      return character.name.toLowerCase().includes(filterByName.toLowerCase());
+    })
+    .filter((character) => {
+      if (filterByEpisode === 0) {
+        return true;
+      } else {
+        return character.episodes === parseInt(filterByEpisode);
+      }
+    });
 
   const { pathname } = useLocation();
 
@@ -55,14 +48,26 @@ function App() {
 
   return (
     <div className="App">
-      <img src="https://i.pinimg.com/originals/be/42/d8/be42d893690195636c47a571b70b4190.jpg" alt="Rick & Morty" className="title"/>
+      <img
+        src="https://i.pinimg.com/originals/be/42/d8/be42d893690195636c47a571b70b4190.jpg"
+        alt="Rick & Morty"
+        className="title"
+      />
       <Routes>
         <Route
           path="/"
           element={
             <>
-              <Filters handleFilterName={handleFilterName} filterByName={filterByName} handleNumberEpisode={handleNumberEpisode} filterByEpisode={filterByEpisode} />
-              <CharactersList characters={CharactersFiltered} filterByName={filterByName} />
+              <Filters
+                handleFilterName={handleFilterName}
+                filterByName={filterByName}
+                handleNumberEpisode={handleNumberEpisode}
+                filterByEpisode={filterByEpisode}
+              />
+              <CharactersList
+                characters={CharactersFiltered}
+                filterByName={filterByName}
+              />
             </>
           }
         />
